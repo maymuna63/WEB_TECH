a4141111777dadabcb1937980501164c09efdd3a
maymuna63 <maymunajannat63@gmail.com>
2025-11-17 23:42:22 +0600

    Create facilities.php

---
+++ facilities.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" charset="width=device-width,initial-scale=1.0">
+    <title>HotelEasy - FACILITIES</title>
+    
+  
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css"/>
+    <?php require('inc/links.php'); ?>
+<style>
+    .pop:hover{
+        border-top-color: var(--teal) !important;
+        transform: scale(1.03);
+        transition: all 0.3s;
+    }
+</style>
+</head>
+<body class="bg-light">
+  
+<?php require('inc/header.php'); ?>
+
+<div class="my-5 px-4">
+    <h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
+    <div class="h-line bg-dark"></div>
+    <p class="text-center mt-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+       Adipisci perspiciatis fugit doloribus ab dignissimos maxime <br>
+        facilis sit beatae delectus quas aspernatur molestiae, 
+        assumenda suscipit excepturi. Soluta et impedit eius id?</p>
+</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 bordar-derk pop">
+                    <div class="d-flex align-items-center mb-2">
+                    <img src="images/facilities/IMG_47816.svg" width="40px">
+                    <h5 class="m-0 ms-3">WIFI</h5>
+                    </div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat quae aliquid voluptate aut itaque consequuntur eaque possimus. Repudiandae fugiat impedit nisi autem.</p>
+                
+            </div>
+        </div>
+                    <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 bordar-derk pop">
+                    <div class="d-flex align-items-center mb-2">
+                    <img src="images/facilities/IMG_47816.svg" width="40px">
+                    <h5 class="m-0 ms-3">WIFI</h5>
+                    </div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat quae aliquid voluptate aut itaque consequuntur eaque possimus. Repudiandae fugiat impedit nisi autem.</p>
+                
+            </div>
+        </div>
+                    <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 bordar-derk pop">
+                    <div class="d-flex align-items-center mb-2">
+                    <img src="images/facilities/IMG_47816.svg" width="40px">
+                    <h5 class="m-0 ms-3">WIFI</h5>
+                    </div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat quae aliquid voluptate aut itaque consequuntur eaque possimus. Repudiandae fugiat impedit nisi autem.</p>
+                
+            </div>
+        </div>
+                    <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 bordar-derk pop">
+                    <div class="d-flex align-items-center mb-2">
+                    <img src="images/facilities/IMG_96423.svg" width="40px">
+                    <h5 class="m-0 ms-3">WIFI</h5>
+                    </div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat quae aliquid voluptate aut itaque consequuntur eaque possimus. Repudiandae fugiat impedit nisi autem.</p>
+                
+            </div>
+        </div>
+                    <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 bordar-derk pop">
+                    <div class="d-flex align-items-center mb-2">
+                    <img src="images/facilities/IMG_27079.svg" width="40px">
+                    <h5 class="m-0 ms-3">WIFI</h5>
+                    </div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat quae aliquid voluptate aut itaque consequuntur eaque possimus. Repudiandae fugiat impedit nisi autem.</p>
+                
+            </div>
+        </div>
+                    <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 bordar-derk pop">
+                    <div class="d-flex align-items-center mb-2">
+                    <img src="images/facilities/IMG_47816.svg" width="40px">
+                    <h5 class="m-0 ms-3">WIFI</h5>
+                    </div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat quae aliquid voluptate aut itaque consequuntur eaque possimus. Repudiandae fugiat impedit nisi autem.</p>
+                
+            </div>
+        </div>
+    </div>
+</div>
+<?php require('inc/foother.php'); ?>
+
+
+</body>
+</html>
